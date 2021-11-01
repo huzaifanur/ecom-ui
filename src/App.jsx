f0821f110acde92591be8276632b21f81c52e45d
huzaifanu <huzaifa98nur@gmail.com>
2021-11-01 17:17:08 +0500

    store UI

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/jsx-no-undef */
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  ${mobile({ height: "30vh" })}
+`;
+const Info = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  top: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+
+  cursor: pointer;
+  font-weight: 600;
+`;
+
+export default function CategoryItem({ item }) {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
+}
